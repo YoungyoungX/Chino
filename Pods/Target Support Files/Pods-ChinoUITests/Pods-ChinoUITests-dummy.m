761ae4d93d2dc5292cc6357fb00a99ad3b5e7879
Shiyangyang <shiyangyang@yeecall.com>
2017-03-29 22:43:59 +0800

    change: update pods

---
+++ Pods/Target Support Files/Pods-ChinoUITests/Pods-ChinoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChinoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ChinoUITests
+@end
---
+++ Pods/Target Support Files/TAPromotee/TAPromotee-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "TACloseButton.h"
+#import "TAITunesClient.h"
+#import "TAPromotee.h"
+#import "TAPromoteeApp.h"
+#import "TAPromoteeViewController.h"
+
+FOUNDATION_EXPORT double TAPromoteeVersionNumber;
+FOUNDATION_EXPORT const unsigned char TAPromoteeVersionString[];
+
